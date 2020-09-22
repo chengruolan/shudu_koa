596cc0ebdb8cab0778c6398edd0c7cb0bb8d986e
chengruolan <chengruolan@digitalgd.com.cn>
2020-09-22 18:00:40 +0800

    1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = require('koa')()
   , json = require('koa-json')
   , views = require('koa-views')
   , onerror = require('koa-onerror');
+  const router = require('koa-simple-router')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +27,21 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+app.use(router (_ =>{
+  _.get('/' , (ctx, next) => {
+    ctx.body = 'hellow world';
+  })
+
+  _.post('/path' , (ctx, next) => {
+    //ctx.body = 'hellow world';
+  })
+
+  _.all('/login' , (ctx, next) => {
+    ctx.body = 'hellow world';
+  })
+
+}))
+
 app.use(require('koa-static')(__dirname + '/public'));
 
 // routes definition
@@ -35,6 +51,10 @@ app.use(users.routes(), users.allowedMethods());
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
+});
+
+app.listen(3000,()=>{
+  console.log('服务已启动，在 http://localhost:3000/');
 });
 
 module.exports = app;
